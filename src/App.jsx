18b8c433a8c1e50b7887190056da44a9b0c474ce
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-25 16:36:27 +0300

    complete dynamic inputs

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,29 @@ import "./App.css";
 function App() {
   const [serviceList, setServiceList] = useState([{ service: "" }]);
 
+  console.log(serviceList);
+
   //Helper Method to add service
   const handleServiceAdd = () => {
     setServiceList([...serviceList, { service: "" }]);
   };
+
+  //Helper function to remove service button
+
+  const handleServiceRemove = (index) => {
+    const list = [...serviceList]
+    list.splice(index, 1);
+    setServiceList(list)
+  }
+
+  const handleServiceChange = (e, index) => {
+    const { name, value } = e.target
+    const list = [...serviceList]
+    list[index][name] = value;
+    setServiceList(list);
+
+
+  }
 
   return (
     <form autoComplete="off" className="App">
@@ -18,7 +37,7 @@ function App() {
         {serviceList.map((singleService, index) => (
           <div key={index} className="services">
             <div className="first-division">
-              <input className="service" id="service" required type="text" />
+              <input className="service" id="service" required type="text" name="service" value={singleService.service} onChange={(e) => handleServiceChange(e, index)} />
 
               {/* Conditionally render add button to appear once */}
               {serviceList.length - 1 === index && serviceList.length < 4 && (
@@ -31,7 +50,8 @@ function App() {
               {/* conditionally render remove button to appear with more than one input */}
 
               {serviceList.length > 1 && (
-                <button type="button" className="remove-btn">
+                <button type="button" className="remove-btn" 
+                onClick={() => handleServiceRemove(index)}>
                   <span>Remove</span>
                 </button>
               )}
@@ -41,6 +61,13 @@ function App() {
       </div>
       <div className="output">
         <h2>Output</h2>
+        {serviceList.map((singleService, index ) => (
+          <ul key={index}>
+            {singleService.service &&(
+              <li>{singleService.service}</li>
+            )}
+          </ul>
+        ))}
       </div>
     </form>
   );
