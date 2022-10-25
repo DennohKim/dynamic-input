3da09dfa972e78c136b9f6ea69438831cffd1420
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-25 16:05:21 +0300

    implement add service

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,46 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [serviceList, setServiceList] = useState([{ service: "" }]);
+
+  //Helper Method to add service
+  const handleServiceAdd = () => {
+    setServiceList([...serviceList, { service: "" }]);
+  };
 
   return (
-    
-      <form autoComplete="off" className="App">
-        <div className="form-field">
-          <label htmlFor="">Service(s)</label>
-          <div className="services">
+    <form autoComplete="off" className="App">
+      <div className="form-field">
+        <label htmlFor="">Service(s)</label>
+
+        {serviceList.map((singleService, index) => (
+          <div key={index} className="services">
             <div className="first-division">
               <input className="service" id="service" required type="text" />
-              <button type="button" className="add-btn">
-                <span>Add a service</span>
-              </button>
+
+              {/* Conditionally render add button to appear once */}
+              {serviceList.length - 1 === index && serviceList.length < 4 && (
+                <button type="button" className="add-btn" onClick={handleServiceAdd}>
+                  <span>Add a service</span>
+                </button>
+              )}
             </div>
             <div className="second-division">
-              <button type="button" className="remove-btn">
-                <span>Remove</span>
-              </button>
+              {/* conditionally render remove button to appear with more than one input */}
+
+              {serviceList.length > 1 && (
+                <button type="button" className="remove-btn">
+                  <span>Remove</span>
+                </button>
+              )}
             </div>
           </div>
-        </div>
-        <div className="output">
-          <h2>Output</h2>
-        </div>
-      </form>
-    
+        ))}
+      </div>
+      <div className="output">
+        <h2>Output</h2>
+      </div>
+    </form>
   );
 }
 
